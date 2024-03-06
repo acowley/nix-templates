50202b8e6b076ec29b76986f94253f6bd8068e63
Anthony Cowley <acowley@gmail.com>
2024-03-06 08:33:18 -0500

    Simplify the basic flake template
    Remove the flake-utils input as we were using very little from
    it.

--- flake-hello/flake.nix
+++ flake-hello/flake.nix
@@ -1,17 +1,23 @@
+# If you wish to make use of an overlay from an input flake, change the expression
+# `nixpkgs.legacyPackages.${system}` to
+# `(import nixpkgs { inherit system overlay; })`
 {
   description = "A very basic flake";
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-23.05-darwin;
-    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: 
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
+  outputs = { self, nixpkgs }:
+    let forAllSystems = function:
+          nixpkgs.lib.genAttrs [
+            "x86_64-linux" "aarch64-linux"
+            "x86_64-darwin" "aarch64-darwin"
+          ] (system: function nixpkgs.legacyPackages.${system});
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
           buildInputs = [ ];
         };
-      }
-    );
+      });
+    };
 }
