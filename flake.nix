2bda34edcb499db888a75188c9910ac5f7714d3d
Anthony Cowley <acowley@gmail.com>
2024-08-20 17:31:28 -0400

    Add rust-hello to flake outputs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
         path = ./haskell-hello;
         description = "Haskell package development flake with HLS";
       };
+      rust-hello = {
+        path = ./rust-hello;
+        description = "Rust package development flake with rust-analyzer";
+      };
       flake-hello = {
         path = ./flake-hello;
         description = "Most basic flake with flake-utils";
