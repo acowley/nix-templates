df5ef12a92352bcc2ff0408afa9f1df9caf64cbe
Anthony Cowley <acowley@gmail.com>
2024-09-29 20:24:00 -0400

    Update flake-hello
    - Update the default `nixpkgs.url`
    - Include an example `packages` output

--- flake-hello/flake.nix
+++ flake-hello/flake.nix
@@ -3,21 +3,25 @@
 # `(import nixpkgs { inherit system overlay; })`
 {
   description = "A very basic flake";
-  inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-23.05-darwin;
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05"; };
 
   outputs = { self, nixpkgs }:
-    let forAllSystems = function:
-          nixpkgs.lib.genAttrs [
-            "x86_64-linux" "aarch64-linux"
-            "x86_64-darwin" "aarch64-darwin"
-          ] (system: function nixpkgs.legacyPackages.${system});
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      nixpkgsFor = system: nixpkgs.legacyPackages.${system};
     in {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = [ ];
-        };
+      devShells = forAllSystems (system: {
+        default = (nixpkgsFor system).mkShell { buildInputs = [ ]; };
       });
+      ## Example packages output
+      # packages = forAllSystems (system: rec {
+      #   myPackage = (nixpkgsFor system).callPackages ./myPackage.nix {};
+      #   default = myPackage;
+      # });
     };
 }
